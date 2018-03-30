5dd141d808834354d753bfb4efc8afab5e895ea5
gwangjar <gwangjar@naver.com>
2018-03-30 13:05:11 +0900

    Add files via upload

---
+++ BoardDAO.java
@@ -0,0 +1,176 @@
+package mycgv.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import mycgv.vo.BoardVO;
+
+public class BoardDAO {
+	// Field
+
+	PreparedStatement pstmt;
+	ResultSet rs;
+
+	// Constructor
+	public BoardDAO() {
+		
+		
+	}
+
+	// Method
+	public void getPreparedStatement(String sql) {
+		try {
+			pstmt = DBConn.getConnection().prepareStatement(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// insert query: insert into cgv_board
+	// values(sequ_cgv_board.nextval,?,?,?,sysdate,0)
+	public int getInsertResult(BoardVO vo) {
+		int result = 0;
+		try {
+			String sql = "insert into cgv_board values(sequ_cgv_board.nextval,?,?,?,sysdate,0)";
+			getPreparedStatement(sql);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setString(3, vo.getFname());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+
+	}
+
+	// 전체 리스트 출력(RNO 포함) -page navi 적용
+	public ArrayList<BoardVO> getResultList(int startCount, int endCount) {
+		ArrayList<BoardVO> list = new ArrayList<BoardVO>();
+		try {
+//			String sql = "select rownum as rno, no,title,to_char(bdate,'yyyy/mm/dd'),hits "
+//					+ " from (select * from cgv_board order by bdate desc)";
+			String sql = "select rno,no,title,bdate,hits " 
+					+ "from (select rownum as rno,no,title,to_char(bdate,'yyyy/mm/dd') as bdate,hits "
+					+ "from (select * from cgv_board order by bdate desc))"
+					+ "where rno between ? and ?";
+			getPreparedStatement(sql);
+			pstmt.setInt(1, startCount);
+			pstmt.setInt(2, endCount);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				BoardVO vo = new BoardVO();
+				vo.setRno(rs.getInt(1));
+				vo.setNo(rs.getInt(2));
+				vo.setTitle(rs.getString(3));
+				vo.setBdate(rs.getString(4));
+				vo.setHits(rs.getInt(5));
+				list.add(vo);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+
+	}
+
+	// 한개 LOW 값 가져오기
+	public BoardVO getResultVO(String no) {
+		BoardVO vo = new BoardVO();
+		try {
+			String sql = "select * from cgv_board where no=?";
+			getPreparedStatement(sql);
+			pstmt.setString(1, no);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				vo.setNo(rs.getInt(1));
+				vo.setTitle(rs.getString(2));
+				vo.setContent(rs.getString(3));
+				vo.setFname(rs.getString(4));
+				vo.setBdate(rs.getString(5));
+				vo.setHits(rs.getInt(6));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vo;
+
+	}
+
+	// 조회수 업데이트
+	public void getUpdateHits(String no) {
+		try {
+			String sql = "update cgv_board set hits =hits+1 where no=?";
+			getPreparedStatement(sql);
+			pstmt.setString(1, no);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 게시판 수정하기
+	public int getUpdateResult(BoardVO vo) {
+		int result = 0;
+		try {
+			String sql = "update cgv_board set title=?, content=?, fname=?" + " where no=?";
+			getPreparedStatement(sql);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setString(3, vo.getFname());
+			pstmt.setInt(4, vo.getNo());
+
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+
+	}
+
+	// delete 기능 만들기~
+	public int getDeleteResult(String no) {
+		int result = 0;
+		try {
+			String sql="delete from cgv_board where no=?";
+			getPreparedStatement(sql);
+			pstmt.setString(1, no);
+			result =pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	/*전체 board 카운트 가져오기*/
+	public int execTotalCount(){
+		int result =0;
+		try{
+			String sql = "select count(*) from cgv_board";
+			getPreparedStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				result = rs.getInt(1);
+			}
+		}catch(Exception e){e.printStackTrace();}
+		
+		return result;
+	}
+	
+	// closed
+	public void close() {
+		try {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if(DBConn.conn!=null)DBConn.conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
